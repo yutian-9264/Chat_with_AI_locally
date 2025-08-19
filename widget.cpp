14f6a6594f0ed91abfd09f39d86de35328581e6b
yutian <wqyutian@163.com>
2025-08-19 22:28:35 +0800

    搭建基本前端框架

--- widget.cpp
+++ widget.cpp
@@ -6,6 +6,8 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    this->setWindowTitle("本地离线AI聊天工具");
+    ui->comboBox->addItem("DeepSeek-R1-Distill-Qwen-1.5B-UD-IQ2_M");
 }
 
 Widget::~Widget()
