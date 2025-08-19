d9bf377455e0b53c7f77b31dc3396c7593dd48e8
yutian <wqyutian@163.com>
2025-08-20 01:23:17 +0800

    使用llama-cli.exe来调用model运行正常

--- widget.cpp
+++ widget.cpp
@@ -1,5 +1,7 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QDebug>
+#include <QProcess>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
@@ -8,6 +10,32 @@ Widget::Widget(QWidget *parent)
     ui->setupUi(this);
     this->setWindowTitle("本地离线AI聊天工具");
     ui->comboBox->addItem("DeepSeek-R1-Distill-Qwen-1.5B-UD-IQ2_M");
+
+
+
+    QString prompt;
+    prompt = "hi, how are you?";
+    QString exePath = QCoreApplication::applicationDirPath() + "/llama-cli.exe";
+    QString modelPath = QCoreApplication::applicationDirPath() + "/DeepSeek-R1-Distill-Qwen-1.5B-UD-IQ2_M.gguf";
+
+    QStringList arguments;
+    arguments << "-m" << modelPath << "-p" << prompt;
+
+    QProcess* process = new QProcess(this);
+    process->start(exePath, arguments);
+    process->waitForFinished();
+
+    qDebug() << "stderr:" << process->readAllStandardError();
+
+    int exitCode = process->exitCode();
+    if(exitCode != 0)
+    {
+        qDebug() << "llama-cli运行错误，错误码" << exitCode;
+        qDebug() << "错误输出：" <<process->readAllStandardError();
+    }
+
+    QString data = process->readAllStandardOutput();
+    qDebug() << data;
 }
 
 Widget::~Widget()
